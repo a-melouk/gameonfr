ae17987d1b7ac07333fd522250c63f66d274241a
A-Melouk <abdoumelouk96@gmail.com>
2023-10-09 16:21:24 +0200

    Classe 'active' quand un lien du nav est cliqué

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,3 +19,16 @@ modalBtn.forEach(btn => btn.addEventListener('click', launchModal))
 function launchModal() {
   modalbg.style.display = 'block'
 }
+
+//Fonction pour changer la classe d'un lien quand il est cliqué
+function active_link() {
+  let navbar = document.querySelector('.main-navbar')
+  let children = navbar.children
+  for (let i = 0; i < 5; i++)
+    children[i].addEventListener('click', () => {
+      for (let j = 0; j < 5; j++) children[j].classList.remove('active')
+      children[i].classList.add('active')
+    })
+}
+
+active_link()
